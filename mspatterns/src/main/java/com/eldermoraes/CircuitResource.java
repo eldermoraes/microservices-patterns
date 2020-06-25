5417e553126c20d4a39a7ed453c63a1a7b1a9ecd
Elder Moraes <eldermoraes@Elders-MacBook-Pro.local>
2020-06-25 10:18:04 -0300

    Fixed circuit breaker

--- mspatterns/src/main/java/com/eldermoraes/CircuitResource.java
+++ mspatterns/src/main/java/com/eldermoraes/CircuitResource.java
@@ -5,23 +5,23 @@ import java.time.temporal.ChronoUnit;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
 import org.eclipse.microprofile.faulttolerance.Fallback;
-import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.faulttolerance.Timeout;
 
 @Path("/circuit")
 public class CircuitResource {
     
     @Timeout(unit = ChronoUnit.MILLIS, value = 500)
-    //@Retry(maxRetries = 3, delayUnit = ChronoUnit.MILLIS, delay = 500)
     @Fallback(fallbackMethod = "fallback")
+    @CircuitBreaker(requestVolumeThreshold = 4, failureRatio = 0.5, delay = 2000, delayUnit = ChronoUnit.MILLIS, successThreshold = 2)
     @GET
     public String getCircuit() throws InterruptedException {
-        Thread.sleep(400);
-        return "Circuit";
+        Thread.sleep(600);
+        return "Circuit \n";
     }
 
     public String fallback(){
-        return "I'm a fallback";
+        return "I'm a fallback ";
     }
 }
